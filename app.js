46cd0f48a575d61635f69a03d855aff2bb715455
MatteoLatoile <me2llincontact@gmail.com>
2025-02-08 17:35:15 +0100

    je sais plus

--- app.js
+++ app.js
@@ -1,63 +1,10 @@
-const coords = { x: 0, y: 0 };
-const circles = document.querySelectorAll(".circle");
+let icon = document.querySelector("#icon");
 
-const colors = [
-  "#ffffff",
-  "#ffffff",
-  "#ffffff",
-  "#fff",
-  "#fff",
-  "#fff",
-  "#fff",
-  "#fff",
-  "#fff",
-  "#fff",
-  "#fff",
-  "#fff",
-  "#fff",
-  "#fff",
-  "#fff",
-  "#fff",
-  "#fff",
-  "#fff",
-  "#fff",
-  "#fff",
-  "#fff",
-  "#fff"
-];
-
-circles.forEach(function (circle, index) {
-  circle.x = 0;
-  circle.y = 0;
-  circle.style.backgroundColor = colors[index % colors.length];
-});
-
-window.addEventListener("mousemove", function(e){
-  coords.x = e.clientX;
-  coords.y = e.clientY;
-  
-});
-
-function animateCircles() {
-  
-  let x = coords.x;
-  let y = coords.y;
-  
-  circles.forEach(function (circle, index) {
-    circle.style.left = x - 12 + "px";
-    circle.style.top = y - 12 + "px";
-    
-    circle.style.scale = (circles.length - index) / circles.length;
-    
-    circle.x = x;
-    circle.y = y;
-
-    const nextCircle = circles[index + 1] || circles[0];
-    x += (nextCircle.x - x) * 0.3;
-    y += (nextCircle.y - y) * 0.3;
-  });
- 
-  requestAnimationFrame(animateCircles);
+icon.onclick = function(){
+  document.body.classList.toggle("darkMode");
+  if(document.body.classList.contains("darkMode")){
+    icon.src = ".fa-solid fa-moon";
+  }else{
+    icon.src = ".fa-solid fa-sun";
+  }
 }
-
-animateCircles();
